aabf43d015545eda32ae3b4be80cf5ed67dccf4f
Ose Daniel <danose1991@gmail.com>
2022-01-10 16:12:19 +0100

    prepping for dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-tsparticles';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
+const particlesOptions = {
+    particles: {
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+     
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      }, collisions: {
+        enable: true,
+      },
+     
+     
+    },
+    detectRetina: true,
+  }
 function App() {
+
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Particles className="particles"
+        
+        init={particlesInit}
+        loaded={particlesLoaded} 
+    options={particlesOptions}
+     />
+     <Navigation />
+     <Logo />
+     <Rank />
+     <ImageLinkForm />
+     
+
+    {/* {
+     <FaceRecognition />} */}
     </div>
   );
 }
